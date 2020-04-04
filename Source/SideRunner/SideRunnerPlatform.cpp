b1a1d435e8b46cd531febbf1bb359cbd8a4a3ab1
José Luis <alagunasalahaddin@live.com>
2020-04-04 17:42:17 +0300

    :tada: Created a new C++ Platform actor.
    - Created the default actor with C++.
    - Created a new Platform based on it.
    - Replaced the old Platform actor by this new actor.

---
+++ Source/SideRunner/SideRunnerPlatform.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SideRunnerPlatform.h"
+#include "AI/NavigationSystemBase.h"
+#include "Components/BoxComponent.h"
+#include "Components/ChildActorComponent.h"
+#include "Math/UnrealMathUtility.h"
+#include "PaperSprite.h"
+#include "PaperSpriteComponent.h"
+#include "SideRunnerGameModeBase.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+// Sets default values
+ASideRunnerPlatform::ASideRunnerPlatform()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	Obstacle = CreateDefaultSubobject<UChildActorComponent>(TEXT("Obstacle"));
+	Obstacle->SetupAttachment(RootComponent);
+	Obstacle->CreateChildActor();
+
+	TriggerScore = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerScore"));
+	TriggerScore->SetupAttachment(RootComponent);
+	TriggerScore->SetWorldScale3D(FVector(1.f, 1.f, 15.f));
+	TriggerScore->AreaClass = FNavigationSystem::GetDefaultObstacleArea();
+
+	// There is no a way to use FObjectFinder outside to the constructor
+	static ConstructorHelpers::FObjectFinder<UPaperSprite> BoxSprite(TEXT("/Game/Textures/Sprites/box_Sprite"));
+	check(BoxSprite.Succeeded());
+	BlockSprites.Add(BoxSprite.Object);
+
+	static ConstructorHelpers::FObjectFinder<UPaperSprite> BoxAltSprite(TEXT("/Game/Textures/Sprites/boxAlt_Sprite"));
+	check(BoxAltSprite.Succeeded());
+	BlockSprites.Add(BoxAltSprite.Object);
+	
+	InitialLifeSpan = 4.f;
+}
+
+// Called when the game starts or when spawned
+void ASideRunnerPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Ref_GameMode = Cast<ASideRunnerGameModeBase>(GetWorld()->GetAuthGameMode());
+
+	TilesAmount = bInitialPlatform ? 10 : FMath::RandRange(MinBlocks, MaxBlocks);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Tiles Amount: %d"), TilesAmount);
+
+	FString BlockName;
+
+	for (int i = 0; i < TilesAmount; i++)
+	{
+		BlockName = "Block_" + FString::FromInt(i);
+
+		UPaperSpriteComponent* Block = NewObject<UPaperSpriteComponent>(this, UPaperSpriteComponent::StaticClass(), FName(*BlockName));
+
+		if (Block)
+		{
+			Block->RegisterComponent();
+            Block->AttachTo(this->RootComponent);
+			Block->SetRelativeLocation(FVector(i * 70.f, 0.0f, 0.f));
+			// Block->SetSprite(ConstructorHelpers::FObjectFinder<UPaperSprite> (FMath::RandBool() ? TEXT("/Game/Textures/Sprites/box_Sprite") : TEXT("/Game/Textures/Sprites/boxAlt_Sprite")).Object);
+			Block->SetSprite(FMath::RandBool() ? BlockSprites[0] : BlockSprites[1]);
+		}
+	}
+
+	if(bInitialPlatform){
+		if(Obstacle) Obstacle->DestroyComponent();
+		if(TriggerScore) TriggerScore->DestroyComponent();
+	} else {
+		float XLocation = FMath::RandRange(0, TilesAmount - 1) * 70;
+		if(Obstacle) Obstacle->SetRelativeLocation(FVector(XLocation, 0.f, 35.f));
+		if(TriggerScore) TriggerScore->SetRelativeLocation(FVector(XLocation, 0.f, 0.f));
+	}
+}
+
+// Called every frame
+void ASideRunnerPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AddActorWorldOffset(FVector(DeltaTime * -WorldOffset, 0.f, 0.f));
+}
+
---
+++ Source/SideRunner/SideRunnerPlatform.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SideRunnerPlatform.generated.h"
+
+UCLASS()
+class SIDERUNNER_API ASideRunnerPlatform : public AActor
+{
+	GENERATED_BODY()
+
+	class ASideRunnerGameModeBase* Ref_GameMode;
+
+	/* For further developments: we can choose more than two types os block and add to the platform since the editor */
+	UPROPERTY()
+	TArray<class UPaperSprite *> BlockSprites;
+
+	UPROPERTY(Category=Actor, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	class UBoxComponent* TriggerScore = nullptr;
+
+	UPROPERTY(Category=Actor, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	class UChildActorComponent* Obstacle = nullptr;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category=Platform)
+	int32 MinBlocks = 4;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Platform)
+	int32 MaxBlocks = 8;
+
+	UPROPERTY(EditDefaultsOnly, Category=Platform, meta=(ToolTip="It will be get as a negative value"))
+	float WorldOffset = 500.f;
+
+
+public:	
+	// Sets default values for this actor's properties
+	ASideRunnerPlatform();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly, Category=Platform)
+	int TilesAmount;
+
+	UPROPERTY(BlueprintReadOnly, Category=Platform, Meta = (ExposeOnSpawn = true))
+	bool bInitialPlatform;
+
+	UPROPERTY(BlueprintReadWrite, Category=Platform)
+	bool bSpawnNext;
+
+};
